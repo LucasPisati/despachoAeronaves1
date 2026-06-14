82cc2b63bce8716d43b82b53e8df960957e9030d
Lucas Pisati <41471326@comunidadort.edu.ar>
2026-06-14 15:04:00 -0300

    agregar configuracion de variables de sesion y cookies en Program.cs

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Diagnostics;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -5,6 +6,7 @@ using Microsoft.EntityFrameworkCore;
 using despachoAeronave.Models;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Security.Claims;
 
 namespace despachoAeronave.Controllers
 {
@@ -20,16 +22,42 @@ namespace despachoAeronave.Controllers
 
         public async Task<IActionResult> Index()
         {
-            ViewBag.TotalFlights = await _context.Vuelos.CountAsync();
+            var isPilot = User.IsInRole("Piloto");
+            
+            // Get logged in pilot user ID from claims
+            var userIdClaim = User.FindFirst("UserId")?.Value;
+            int? loggedInUserId = null;
+            if (int.TryParse(userIdClaim, out var id))
+            {
+                loggedInUserId = id;
+            }
+
+            IQueryable<Vuelo> flightsQuery = _context.Vuelos;
+            IQueryable<Despacho> dispatchesQuery = _context.Despachos;
+
+            if (isPilot && loggedInUserId.HasValue)
+            {
+                flightsQuery = flightsQuery.Where(v => v.PilotoId == loggedInUserId.Value);
+                dispatchesQuery = dispatchesQuery.Where(d => d.Vuelo!.PilotoId == loggedInUserId.Value);
+            }
+
+            ViewBag.TotalFlights = await flightsQuery.CountAsync();
             ViewBag.ActiveAircraft = await _context.Aeronaves.CountAsync(a => a.Estado == "Activa");
-            ViewBag.TotalDispatches = await _context.Despachos.CountAsync();
+            ViewBag.TotalDispatches = await dispatchesQuery.CountAsync();
 
             ViewBag.DispatchedFlightIds = await _context.Despachos.Select(d => d.VueloId).ToListAsync();
+            
+            // Retrieve dispatches details to map signatures on the home view
+            ViewBag.PilotSignedFlightIds = await _context.Despachos
+                .Where(d => d.EstaAprobadoPorPiloto)
+                .Select(d => d.VueloId)
+                .ToListAsync();
 
-            var recentFlights = await _context.Vuelos
+            var recentFlights = await flightsQuery
                 .Include(v => v.Aeronave)
+                .Include(v => v.Piloto)
                 .OrderBy(v => v.FechaHoraSalida)
-                .Take(5)
+                .Take(10)
                 .ToListAsync();
 
             return View(recentFlights);
